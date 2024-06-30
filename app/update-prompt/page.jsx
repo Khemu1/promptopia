6a3e42dc8729471c4d0a0a62d0d87a2340ef2fcb
Khemu <alghost2004@gmail.com>
2024-06-30 09:54:24 +0300

    .

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState, useEffect, Suspense } from "react";
-import { useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Form from "@components/Form";
 
